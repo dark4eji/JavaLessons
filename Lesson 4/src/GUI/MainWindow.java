66254e155a2b1b8d10c2af3c515ec5f31a82ec2e
SavinAV <dark4eji@gmail.com>
2019-03-03 02:44:10 +0700

    Add Lesson 4 (GUI) code

---
+++ Lesson 4/src/GUI/MainWindow.java
@@ -0,0 +1,112 @@
+package GUI;
+
+import core.MessageOperator;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class MainWindow extends JFrame {
+    private final int WIN_MARGIN = 4;
+
+    private JPanel showMessagesPanel = new JPanel();
+    private JPanel sendMessagesPanel = new JPanel();
+
+    private JMenuBar menuBar = new JMenuBar();
+    private JMenu actionsMenu = new JMenu("Действия");
+    //private JMenu settingsMenu = new JMenu("Настройки");
+
+    private JMenuItem save = new JMenuItem("Сохранить переписку");
+    private JMenuItem exit = new JMenuItem("Выход");
+
+    private JTextArea textArea = new JTextArea();
+    private JTextField textField = new JTextField();
+    private JButton sendButton = new JButton("Отправить");
+    private Container container = getContentPane();
+    private Font messagesFont = new Font("Segoe UI", Font.PLAIN, 14);
+    private JScrollPane scroll = new JScrollPane();
+
+    public MainWindow(){
+        super("Chat 0.1");
+        setVisible(true);
+        setSize(600, 700);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        // Добавление панели меню
+        actionsMenu.add(save);
+        actionsMenu.addSeparator();
+        actionsMenu.add(exit);
+
+        menuBar.add(actionsMenu);
+        //menuBar.add(settingsMenu);
+
+        setJMenuBar(menuBar);
+
+        textArea.setFont(messagesFont); // Установка кастомного шрифта
+        textArea.setLineWrap(true);
+        textArea.setEditable(false);
+
+        textField.grabFocus();
+
+        scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        scroll.setViewportView(textArea);
+
+        showMessagesPanel.setLayout(new BoxLayout(showMessagesPanel, BoxLayout.PAGE_AXIS));
+        showMessagesPanel.add(scroll);
+
+        showMessagesPanel.setBorder(BorderFactory.createEmptyBorder(WIN_MARGIN,
+                WIN_MARGIN,
+                WIN_MARGIN,
+                WIN_MARGIN));
+
+        sendMessagesPanel.setLayout(new BoxLayout(sendMessagesPanel, BoxLayout.LINE_AXIS));
+        sendMessagesPanel.add(textField);
+        sendMessagesPanel.add(sendButton);
+        sendMessagesPanel.setBorder(BorderFactory.createEmptyBorder(0,
+                WIN_MARGIN,
+                WIN_MARGIN,
+                WIN_MARGIN));
+
+        container.add(showMessagesPanel, BorderLayout.CENTER);
+        container.add(sendMessagesPanel, BorderLayout.PAGE_END);
+
+        // Слушатель на отправку сообщения через кнопку
+        sendButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                MessageOperator.sendMessage(textField, textArea);
+            }
+        });
+
+        // Слушатель на отправку сообщений через клавишу ENTER
+        textField.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    MessageOperator.sendMessage(textField, textArea);
+                }
+            }
+        });
+
+        // Слушатель на сохранение текущей беседы в файл
+        save.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                MessageOperator.saveCurrentChat(textArea);
+            }
+        });
+
+
+        // Слушатель на выход из программы
+        exit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+}
+
+
+
+
