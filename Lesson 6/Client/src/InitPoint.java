327540d505cfe7ace4fec354035595d10bf91907
SavinAV <dark4eji@gmail.com>
2019-03-03 17:19:01 +0700

    Prepare environment for projects

---
+++ Lesson 6/Client/src/InitPoint.java
@@ -0,0 +1,7 @@
+import GUI.MainWindow;
+
+public class InitPoint {
+    public static void main(String[] args) {
+        new MainWindow();
+    }
+}
---
+++ Lesson 6/Server/src/InitPoint.java
@@ -0,0 +1,7 @@
+import core.EchoServer;
+
+public class InitPoint {
+    public static void main(String[] args) {
+        EchoServer.startServer();
+    }
+}
